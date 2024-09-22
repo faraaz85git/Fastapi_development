c362c078fe1cb8ff723b57767edcf69f8f11877d
Faraaz Zaidi <faraazzaidi85@gmail.com>
2024-09-22 22:14:25 +0530

    Api supporting query parameter

--- book1.py
+++ book1.py
@@ -4,7 +4,7 @@ app = FastAPI()
 Books=[
     {'title':'One','author':'Author one','category':'one'},
     {'title':'Two','author':'Author one','category':'one'},
-    {'title': 'Three', 'author': 'Author one', 'category': 'one'},
+    {'title': 'Three', 'author': 'Author one', 'category': 'two'},
 
 ]
 
@@ -22,9 +22,18 @@ async def get_book():
         if book['title']=='One':
             return book
 
-@app.get('/Books/{title}')
+@app.get('/Books/{title}') #anything after Book is path parameter and title is pointing to it.
 async def get_book(title):
     for book in Books:
         if book['title']==title:
             return book
+
+
+@app.get('/Books/') #anything after Book is now query parameter
+async def get_book_by_category(category:str):
+    books=[]
+    for book in Books:
+        if book.get('category')==category:
+            books.append(book)
+    return books
 
